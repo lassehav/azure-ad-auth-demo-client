6717fd36e41169ec88b5397e57d3786e91d1d635
Lasse Haverinen <lasse.haverinen@gmail.com>
2017-10-11 13:25:00 +0300

    express server for static content

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+
+
+app.use(express.static('static'))
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+
+app.get('/test', function (req, res) {
+    res.send({
+        id: 1,
+        description: "Hello World"
+    });
+});
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port 3000!')
+})

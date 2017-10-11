51131e24d6a4b85b8d75d12aa07c00ea355880a3
Lasse Haverinen <lasse.haverinen@gmail.com>
2017-10-11 22:42:36 +0300

    Working app & api authentication with localhost implemented

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 
-//app.use(express.static('static'))
+app.use(express.static('static'))
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
